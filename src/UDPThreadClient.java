1642c58e38cca817136bab158b80c7fbf21190c8
Peilun <peilun2016@gmail.com>
2023-04-03 22:34:14 +0800

    update

--- src/UDPThreadClient.java
+++ src/UDPThreadClient.java
@@ -1,28 +1,36 @@
+
+// 引入 DatagramPacket、DatagramSocket 和 InetAddress 類別
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
 
 public class UDPThreadClient {
     public static void main(String[] args) {
         try (DatagramSocket socket = new DatagramSocket()) {
-            Scanner scanner = new Scanner(System.in);
-            String message = "";
-            while (!message.equals("exit")) {
-                System.out.print("Enter message: ");
-                message = scanner.nextLine();
-                byte[] buffer = message.getBytes();
-                InetAddress address = InetAddress.getByName("127.0.0.1");
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 5005);
-                socket.send(packet);
-                buffer = new byte[1024];
-                packet = new DatagramPacket(buffer, buffer.length);
-                socket.receive(packet);
-                String response = new String(packet.getData(), 0, packet.getLength());
-                System.out.println("Received response from " + packet.getAddress().getHostAddress() + ":"
-                        + packet.getPort() + ": " + response);
-            }
+            // 設定要發送的訊息為 "request mow time"，並將其轉換為 byte 陣列
+            byte[] buffer = "request mow time".getBytes();
+            // 設定要傳送到的 IP 位址為 "127.0.0.1"，即本機 IP 位址
+            InetAddress address = InetAddress.getByName("127.0.0.1");
+            // 建立 DatagramPacket 物件，用來包裝要傳送的資料、資料長度、目的地 IP 位址和埠號
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4010);
+            // 使用 socket 的 send() 方法傳送資料
+            socket.send(packet);
+            // 建立新的 byte 陣列，用來接收回應訊息
+            buffer = new byte[1024];
+            // 建立 DatagramPacket 物件，用來接收回應資料
+            packet = new DatagramPacket(buffer, buffer.length);
+            // 使用 socket 的 receive() 方法接收回應資料
+            socket.receive(packet);
+            // 將接收到的 byte 陣列轉換為字串，並顯示回應訊息
+            String response = new String(packet.getData(), 0, packet.getLength());
+            String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+
+            System.out.println(
+                    "[" + timeStamp + "] Received response from server " + packet.getAddress().getHostAddress() + ":"
+                            + packet.getPort() + ":" + "\n ---> " + response);
         } catch (Exception ex) {
+            // 若有錯誤發生，印出錯誤訊息
             ex.printStackTrace();
         }
     }
--- src/UDPThreadServer.java
+++ src/UDPThreadServer.java
@@ -1,21 +1,44 @@
+
+// 匯入java.net套件的DatagramPacket與DatagramSocket類別
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.text.SimpleDateFormat;
 
 public class UDPThreadServer {
     public static void main(String[] args) {
-        try (DatagramSocket socket = new DatagramSocket(5005)) {
+        try (
+                // 建立DatagramSocket物件，監聽本機4010埠
+                DatagramSocket socket = new DatagramSocket(4010)) {
+            // 設定緩衝區大小為1024位元組
             byte[] buffer = new byte[1024];
+            // 重複接收訊息與回應
             while (true) {
+                // 建立DatagramPacket物件，傳入緩衝區與大小
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                // 接收訊息，將訊息儲存至packet物件中
                 socket.receive(packet);
+                // 將接收到的訊息轉成字串
                 String message = new String(packet.getData(), 0, packet.getLength());
-                System.out.println("Received message from " + packet.getAddress().getHostAddress() + ":"
-                        + packet.getPort() + ": " + message);
-                String response = "Hello " + packet.getAddress().getHostAddress() + ":" + packet.getPort() + "!";
+                // 顯示接收到的訊息
+                String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+                System.out
+                        .println("[" + timeStamp + "] Received message from " + packet.getAddress().getHostAddress()
+                                + ":"
+                                + packet.getPort() + ": " + message);
+                // 建立java.util.Date物件，取得目前時間
+                java.util.Date date = new java.util.Date();
+                // 將目前時間轉成字串
+                String dateString = date.toString();
+                // 設定回應訊息為現在時間
+                String response = "Now Time " + dateString;
+                // 將回應訊息轉成位元組
                 buffer = response.getBytes();
+                // 建立DatagramPacket物件，傳入回應訊息、位元組大小、IP位址、埠號
                 packet = new DatagramPacket(buffer, buffer.length, packet.getAddress(), packet.getPort());
+                // 發送回應訊息
                 socket.send(packet);
             }
+            // 處理例外狀況
         } catch (Exception ex) {
             ex.printStackTrace();
         }
